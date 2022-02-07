fe3cd237a5616924017ea8a3367ef39e17b30056
viniciusdsv93 <viniciusdsv93@gmail.com>
2022-02-07 08:24:05 -0300

    Corrected multi and division simbols

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-document.querySelectorAll('.buttons span').forEach((item) => {
+function qs(item) {
+    return document.querySelector(item);
+}
+
+function qsall(item) {
+    return document.querySelectorAll(item);
+}
+
+qsall('.buttons span').forEach((item) => {
     item.addEventListener('click', (e) => {
         insertValue(item.innerHTML);
     })
@@ -6,26 +14,26 @@ document.querySelectorAll('.buttons span').forEach((item) => {
 
 function insertValue(data) {
     if (data == "CE") {
-        document.querySelector('input').value = '';
+        qs('input').value = '';
     }
     else if (data == "=") {
         try {
-            document.querySelector('input').value = eval(document.querySelector('input').value);
-            document.querySelector('input').setAttribute('status', 'done');
+            qs('input').value = eval(qs('input').value.replaceAll('x', '*').replaceAll('÷', '/'));
+            qs('input').setAttribute('status', 'done');
         }
         catch(err) {
             console.log("Error: " + err);
-            document.querySelector('input').value = '';
+            qs('input').value = '';
         }
     }
     else {
-        if (document.querySelector('input').getAttribute('status') == 'undone') {
-            document.querySelector('input').value += data;
+        if (qs('input').getAttribute('status') == 'undone') {
+            qs('input').value += data;
         }
         else {
-            document.querySelector('input').value = '';
-            document.querySelector('input').value += data;
-            document.querySelector('input').setAttribute('status', 'undone');
+            qs('input').value = '';
+            qs('input').value += data;
+            qs('input').setAttribute('status', 'undone');
         }
     }
 }
